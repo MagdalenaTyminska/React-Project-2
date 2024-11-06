6f08245b5bf50ec206877cfd8cebec1b84792e6f
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-11-06 21:00:56 +0100

    configuration

---
+++ .history/vite.config_20241106205703.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	base: 'https://magdalenatyminska.github.io/React-Project-1/',
+	server: {
+		port: 5000,
+	},
+	css: {
+		devSourcemap: true,
+	},
+});
